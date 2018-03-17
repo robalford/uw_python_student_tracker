4345d2bc9404393490b71793ddb6b89d8c5acf92
Robert Alford <robertsalford@gmail.com>
2018-03-17 09:58:37 -0700

    StudentTracker model and migration

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class StudentTracker(models.Model):
+    student_tracking_csv = models.FileField(upload_to='tracking_files/')
+    grade_report_csv = models.FileField(upload_to='tracking_files/')
+    date_recorded = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.date_recorded
