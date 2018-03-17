1f4e2bc9f984d68ee1a9db532859807dea77955a
Robert Alford <robertsalford@gmail.com>
2018-03-17 09:30:52 -0700

    Initial commit of student_tracking pinax project

---
+++ manage.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "student_tracking.settings")
+
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
